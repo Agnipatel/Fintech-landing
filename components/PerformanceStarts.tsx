fe2157eb7babaf9e64c91edfb2e47aec527937ac
Er. Agni patel <141851763+Agnipatel@users.noreply.github.com>
2026-07-01 16:36:04 +0530

    Change badge color from green to cyan

--- components/PerformanceStarts.tsx
+++ components/PerformanceStarts.tsx
@@ -49,7 +49,7 @@ export default function WhyPerformanceMarketing() {
 
         <div className="flex justify-center">
           <span className="inline-flex items-center rounded-full border border-green-600 bg-green-600 1px-4 py-2 text-xs font-semibold text-green-600 sm:px-5 sm:text-sm">
-            <span className="mr-2 h-2 w-2 rounded-full bg-green-600"></span>
+            <span className="mr-2 h-2 w-2 rounded-full bg-cyan-600"></span>
             Scale Your Financial Platform Active User Base
           </span>
         </div>
